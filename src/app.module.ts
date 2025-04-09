b117f1c376a5bdc5690561974cfa6d2820e26a6d
Sourabh <sourabhgaikwad20001@gmail.com>
2025-04-09 19:03:33 +0530

    dependency Injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
-
+import { UsersStore } from './users.store';
 
 
 @Module({
   imports: [],
   controllers: [UserController],
-  providers: [],
+  providers: [{provide:UsersStore,useClass:UsersStore},{provide:'DATABASE_NAME',useValue:'Moon_Knight'},{provide:'MAIL',useValue:['admin@domin.com','no-replay@domin.com']}],
 })
 export class AppModule {}
--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,46 +1,63 @@
-import { Body, Controller, Delete, Get, Header, HttpCode, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Header, HttpCode, Inject, Param, Post, Put } from "@nestjs/common";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { UsersStore } from "./users.store";
 
 @Controller('/user')
 export class UserController{
 
-     USERS: CreateUserDto[] = [];
 
-    @Get('/profile')
-    @HttpCode(201)
-    getProfile(){
-        return this.USERS
-    }
 
-    @Post('/add')
-    @Header('cache-control', 'none')
-    @Header('x-control','none')
-    postProfile( @Body() createUserDto:CreateUserDto){
-        this.USERS.push(createUserDto);
-        return "User Added Successfully"
-    }
+//    constructor(store:UsersStore){
+//     console.log(store);
+//    }
+//    constructor(@Inject('DATABASE_NAME') private dbname:string){
+//      console.log(dbname);
+//    }
 
-    @Put('/update/:id')
-    putProfile(@Param('id') id: string, @Body() updatedUserDto: CreateUserDto) {
-        const userIndex = this.USERS.findIndex(user => user.id === +id); // Convert to number with +
-        if (userIndex === -1) {
-            return 'User not found';
-        }
+
+constructor(@Inject('MAIL') private mail:string[])
+{
+    console.log(mail);
+}
+
+
+    //  USERS: CreateUserDto[] = [];
+
+    // @Get('/profile')
+    // @HttpCode(201)
+    // getProfile(){
+    //     return this.USERS
+    // }
+
+    // @Post('/add')
+    // @Header('cache-control', 'none')
+    // @Header('x-control','none')
+    // postProfile( @Body() createUserDto:CreateUserDto){
+    //     this.USERS.push(createUserDto);
+    //     return "User Added Successfully"
+    // }
+
+    // @Put('/update/:id')
+    // putProfile(@Param('id') id: string, @Body() updatedUserDto: CreateUserDto) {
+    //     const userIndex = this.USERS.findIndex(user => user.id === +id); // Convert to number with +
+    //     if (userIndex === -1) {
+    //         return 'User not found';
+    //     }
     
-        this.USERS[userIndex] = updatedUserDto;
-        return 'User updated successfully';
-    }
+    //     this.USERS[userIndex] = updatedUserDto;
+    //     return 'User updated successfully';
+    // }
     
-    @Delete('/delete/:id')
-    deletedProfile(@Param('id') id: string) {
-        const initialLength = this.USERS.length;
-        this.USERS = this.USERS.filter(user => user.id !== +id);
+    // @Delete('/delete/:id')
+    // deletedProfile(@Param('id') id: string) {
+    //     const initialLength = this.USERS.length;
+    //     this.USERS = this.USERS.filter(user => user.id !== +id);
         
-        if (this.USERS.length === initialLength) {
-            return 'User not found';
-        }
+    //     if (this.USERS.length === initialLength) {
+    //         return 'User not found';
+    //     }
     
-        return 'User deleted successfully';
-    }
+    //     return 'User deleted successfully';
+    // }
     
 }
